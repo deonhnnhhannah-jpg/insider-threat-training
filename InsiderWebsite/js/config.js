f0c6c0ac1e61397db020ee8f6c1d05e172edd241
deonhnnhhannah-jpg <deonhnnhhannah@gmail.com>
2026-04-13 23:21:01 -0400

    Delete InsiderWebsite directory

--- InsiderWebsite/js/config.js
+++
@@ -1,4 +0,0 @@
-window.INSIDER_CONFIG = window.INSIDER_CONFIG || {
-  supabaseUrl: "https://tbbnnhmfnenpszfrjssr.supabase.co",
-  supabaseAnonKey: "sb_publishable_yhzEuRj0ZKpXs-axRjn-8g_HcaFcEyN"
-};
